3e9c2e5cf8b12c67db57086fbb9a72437ca641ae
Sayonara Crestani <sayonara.crestani@gmail.com>
2023-04-21 13:26:13 +0200

    Update http-validacao.js

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -3,17 +3,21 @@ import axios from 'axios';
 
 function extractLinks(text) {
     const regex = /https?:\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))/gm;
-    let capturas = [...text.match(regex)];
-    checkStatus(capturas)
+    const capturas = [...text.match(regex)];
+    checkStatus(capturas);
 }
 
-async function checkStatus(urlArr) {
+async function checkStatus(urlArr, method = 'GET') {
     for (const url of urlArr) {
-        const response = await fetch(url);
-        if (response.status === 200) {
+        try {
+            const response = await axios.request({
+                method,
+                url,
+                timeout: 5000 // 5 second timeout
+            });
             console.log(chalk.blue(`Link:`),chalk.white(`${url}`),chalk.green(`- ${response.status} - ${response.statusText}`));
-        } else {
-            console.log(chalk.blue(`Link:`),chalk.white(`${url}`),chalk.red(`- ${response.status} - ${response.statusText}`));
+        } catch (error) {
+            console.log(chalk.blue(`Link:`),chalk.white(`${url}`),chalk.red(`- ${error.message}`));
         }
     }
     console.log(chalk.yellow('Fim do Carregamento.'));
